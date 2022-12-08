1b283b72fe0290ffd1d74f6e2d19490e3d886253
Emilia Hansson <eh222yn@student.lnu.se>
2022-12-08 21:12:52 +0100

    [REFACTOR] Remove implemented constructor to use default constructor instead

--- app/src/main/java/model/Ball.java
+++ app/src/main/java/model/Ball.java
@@ -1,12 +1,5 @@
 package model;
 
 public class Ball {
-    private String size;
-    
-    /**
-     * Creates an instance of a ball with defualt size medium.
-     */
-    public Ball () {
-        size = "medium";
-    }
+
 }
