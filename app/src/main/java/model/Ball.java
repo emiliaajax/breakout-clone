446549e43f6808e63a6ad738958ba53ad58b1430
Emilia Hansson <eh222yn@student.lnu.se>
2022-12-08 21:03:23 +0100

    [REFACTOR] Add comment to clarify that a ball with size medium is created

--- app/src/main/java/model/Ball.java
+++ app/src/main/java/model/Ball.java
@@ -3,6 +3,9 @@ package model;
 public class Ball {
     private String size;
     
+    /**
+     * Creates an instance of a ball with defualt size medium.
+     */
     public Ball () {
         size = "medium";
     }
