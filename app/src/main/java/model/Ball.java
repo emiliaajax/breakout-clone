103751002a6b6cbbfaa57a8f1dae5fbf35221dee
Emilia Hansson <eh222yn@student.lnu.se>
2022-12-09 17:51:17 +0100

    [GREEN] Set heigth in Ball constructor and add a getter for height

--- app/src/main/java/model/Ball.java
+++ app/src/main/java/model/Ball.java
@@ -2,12 +2,18 @@ package model;
 
 public class Ball {
   private int x;
+  private int y;
 
   public Ball () {
     x = 10;
+    y = 10;
   }
 
   public int getWidth() {
     return x;
+  }
+
+  public int getHeight() {
+    return y;
   }
 }
