e91580df12e1b1ae2f5aa7081e36c0c5d7f53a29
Emilia Hansson <eh222yn@student.lnu.se>
2022-12-09 02:41:29 +0100

    [RED] Add test for UI having been initialized

--- app/src/test/java/view/GameUITest.java
+++ app/src/test/java/view/GameUITest.java
@@ -7,12 +7,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class GameUITest {
   class JFrameStub extends JFrame {
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public String title;
+    public boolean resizable;
+    public boolean visible;
+    public String EXIT_ON_CLOSE = "Exited";
+
     public JFrameStub() {
       super();
+    }
+
+    @Override
+    public void setBounds(int x, int y, int width, int height) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+    }
+
+    @Override
+    public void setTitle(String title) {
+      this.title = title;
+    }
+
+    @Override
+    public void setResizable(boolean resizable) {
+      this.resizable = resizable;
+    }
+
+    @Override
+    public void setVisible(boolean visible) {
+      this.visible = visible;
     }
   }
 
   @Test void constructor () {
     assertDoesNotThrow(() -> new GameUI(new JFrameStub()));
+  }
+
+  @Test void shouldReturnTenForWidthOfFrameAfterInitUI () {
+    GameUI sut = new GameUI(new JFrameStub());
+    int expected = 600;
+    int actual = sut.frame.width;
+
+    assertEquals(expected, actual);
   }
 }
