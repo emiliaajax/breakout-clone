61a0e466d1d0a1f052eca82e8ca86d952e67d6c8
Emilia Hansson <eh222yn@student.lnu.se>
2022-12-09 02:22:42 +0100

    [REFACTOR] Refactor to try solve issue with test validating locally but not in pipeline

--- app/src/test/java/view/GameUITest.java
+++ app/src/test/java/view/GameUITest.java
@@ -1,13 +1,16 @@
 package view;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
 import javax.swing.JFrame;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameUITest {
-  class JFrameStub extends JFrame {}
+  class JFrameStub extends JFrame {
+    public JFrameStub() {
+      super();
+    }
+  }
 
   @Test void constructor () {
     assertDoesNotThrow(() -> new GameUI(new JFrameStub()));
