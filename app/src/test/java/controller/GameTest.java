ee172879753e18e8a01e57d334826a4c06f40498
Emilia Hansson <eh222yn@student.lnu.se>
2022-12-09 15:59:10 +0100

    [RED] Add test for creating a Game instance

---
+++ app/src/test/java/controller/GameTest.java
@@ -0,0 +1,10 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+  @Test void constructor() {
+		assertNotNull(new Game());
+	}
+}
