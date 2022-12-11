3b01c39c86a38d31d7cb9ecfd5bd488ce2953623
Emilia Hansson <eh222yn@student.lnu.se>
2022-12-12 00:01:55 +0100

    [GREEN] Add initial x positon when making instance of Brick and implement getter2

--- app/src/main/java/model/Brick.java
+++ app/src/main/java/model/Brick.java
@@ -3,10 +3,12 @@ package model;
 public class Brick {
 	private int width;
 	private int height;
+	private int xPos;
 
 	public Brick () {
 		width = 30;
 		height = 20;
+		xPos = 0;
 	}
 
 	public int getWidth() {
@@ -16,4 +18,8 @@ public class Brick {
 	public int getHeight() {
     return height;
   }
+
+	public int getXPos() {
+		return xPos;
+	}
 }
