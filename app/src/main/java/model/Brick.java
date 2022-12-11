06546e59a73e3db5e3b1025e8a37e7d7206d7369
Emilia Hansson <eh222yn@student.lnu.se>
2022-12-11 23:52:24 +0100

    [GREEN] Implement constructor where height is set to twenty, and implement getter for height

--- app/src/main/java/model/Brick.java
+++ app/src/main/java/model/Brick.java
@@ -2,12 +2,18 @@ package model;
 
 public class Brick {
 	private int width;
+	private int height;
 
 	public Brick () {
 		width = 30;
+		height = 20;
 	}
 
 	public int getWidth() {
     return width;
+  }
+
+	public int getHeight() {
+    return height;
   }
 }
