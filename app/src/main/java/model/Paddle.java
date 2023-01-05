154d56a7cd07dd5217a83747426319b55c8feb01
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-05 19:38:51 +0100

    [GREEN] Add height to constructor and implement a getter

--- app/src/main/java/model/Paddle.java
+++ app/src/main/java/model/Paddle.java
@@ -2,12 +2,18 @@ package model;
 
 public class Paddle {
   private int width;
+  private int height;
 
   public Paddle () {
     width = 150;
+    height = 20;
   }
 
   public int getWidth() {
     return width;
+  }
+
+  public int getHeight() {
+    return height;
   }
 }
