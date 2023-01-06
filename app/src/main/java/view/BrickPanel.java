7b3bc76c180faf0ba346875ae128dfb4affbfc75
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-06 15:39:55 +0100

    [GREEN] Implement paintComponent for oainting a brick

--- app/src/main/java/view/BrickPanel.java
+++ app/src/main/java/view/BrickPanel.java
@@ -4,10 +4,19 @@ import javax.swing.JPanel;
 
 import model.Brick;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class BrickPanel extends JPanel {
   private Brick brick;
 
   public BrickPanel(Brick brick) {
     this.brick = brick;
+  }
+
+  @Override
+  public void paintComponent(Graphics graphics) {
+    graphics.setColor(Color.GREEN);
+    graphics.fillRect(brick.getXPos(), brick.getYPos(), brick.getWidth(), brick.getHeight());
   }
 }
--- app/src/test/java/view/BrickPanelTest.java
+++ app/src/test/java/view/BrickPanelTest.java
@@ -32,6 +32,6 @@ public class BrickPanelTest {
 
     sut.paintComponent(graphics);
 
-    verify(graphics).fillOval(50, 50, 30, 20);
+    verify(graphics).fillRect(50, 50, 30, 20);
   }
 }
