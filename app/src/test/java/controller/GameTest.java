362d7bb08e9a70e556fe27eab22ecf4362f03376
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-07 00:01:05 +0100

    [RED] Add test for creating an instance of Game with Ball, Brick, Paddle and GameUI as arguments

--- app/src/test/java/controller/GameTest.java
+++ app/src/test/java/controller/GameTest.java
@@ -1,10 +1,31 @@
 package controller;
 
 import org.junit.jupiter.api.Test;
+
+import model.Ball;
+import model.Brick;
+import model.Paddle;
+import view.GameUI;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import static org.mockito.Mockito.*;
+
 public class GameTest {
+	private Ball mockBall;
+	private Brick mockBrick;
+	private Paddle mockPaddle;
+	private GameUI mockGameUI;
+
+	@BeforeEach void setUp() {
+		mockBall = mock(Ball.class);
+		mockBrick = mock(Brick.class);
+		mockPaddle = mock(Paddle.class);
+		mockGameUI = mock(GameUI.class);
+	}
+
   @Test void constructor() {
-		assertNotNull(new Game());
+		assertDoesNotThrow(() -> new Game(mockBall, mockBrick, mockPaddle, mockGameUI));
 	}
 }
