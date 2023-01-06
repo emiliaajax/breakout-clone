6f5dea5342b81579d0a2f677eed6bc6afdfa0b9a
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-06 13:05:28 +0100

    [REFACTOR] Create a setUp to prepare for tests

--- app/src/test/java/view/BallPanelTest.java
+++ app/src/test/java/view/BallPanelTest.java
@@ -1,9 +1,23 @@
 package view;
 
 import org.junit.jupiter.api.Test;
+
+import model.Ball;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.awt.Graphics;
+import static org.mockito.Mockito.*;
+
 public class BallPanelTest {
+  @BeforeEach void setUp() {
+    BallPanel sut = new BallPanel();
+    Ball mockBall = mock(Ball.class);
+    Graphics graphics = mock(Graphics.class);
+  }
+
   @Test void constructor () {
     assertDoesNotThrow(() -> new BallPanel());
   }
