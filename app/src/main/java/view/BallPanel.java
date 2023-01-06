967b550327408bdbc26b452afcc9380ef9217163
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-06 13:50:43 +0100

    [REFACTOR] Add that BallPanel should extend JPanel and add Override for paintComponent

--- app/src/main/java/view/BallPanel.java
+++ app/src/main/java/view/BallPanel.java
@@ -3,13 +3,16 @@ package view;
 import model.Ball;
 import java.awt.Graphics;
 
-public class BallPanel {
+import javax.swing.JPanel;
+
+public class BallPanel extends JPanel {
   private Ball ball;
 
   public BallPanel(Ball ball) {
     this.ball = ball;
   }
 
+  @Override
   public void paintComponent(Graphics graphics) {
     graphics.fillOval(ball.getXPos(), ball.getYPos(), ball.getWidth(), ball.getHeight());
   }
