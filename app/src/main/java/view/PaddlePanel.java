21f0ca0a217fd5c15ebe4b72a9a3c74e68c82e16
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-06 15:59:50 +0100

    [GREEN] Implement methods for painting a paddle

--- app/src/main/java/view/PaddlePanel.java
+++ app/src/main/java/view/PaddlePanel.java
@@ -4,10 +4,19 @@ import javax.swing.JPanel;
 
 import model.Paddle;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class PaddlePanel extends JPanel {
   private Paddle paddle;
 
   public PaddlePanel(Paddle paddle) {
     this.paddle = paddle;
+  }
+
+  @Override
+  public void paintComponent(Graphics graphics) {
+    graphics.setColor(Color.BLACK);
+    graphics.fillRect(paddle.getXPos(), paddle.getYPos(), paddle.getWidth(), paddle.getHeight());
   }
 }
