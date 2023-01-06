0d152435129bf624def9e48bbd4d547b68db8f57
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-06 14:15:13 +0100

    [RED] Add test for painting a brick

--- app/src/test/java/view/BrickPanelTest.java
+++ app/src/test/java/view/BrickPanelTest.java
@@ -5,17 +5,33 @@ import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.awt.Graphics;
 
 import model.Brick;
 
 public class BrickPanelTest {
   private Brick mockBrick;
+  private Graphics graphics;
+  private BrickPanel sut;
 
   @BeforeEach void setUp() {
-    Brick mockBrick = mock(Brick.class);
+    mockBrick = mock(Brick.class);
+    sut = new BrickPanel(mockBrick);
+    graphics = mock(Graphics.class);
   }
 
   @Test void constructor() {
     assertDoesNotThrow(() -> new BrickPanel(mockBrick));
+  }
+
+  @Test void paintComponent() {
+    when(mockBrick.getWidth()).thenReturn(30);
+    when(mockBrick.getHeight()).thenReturn(20);
+    when(mockBrick.getXPos()).thenReturn(50);
+    when(mockBrick.getYPos()).thenReturn(50);
+
+    sut.paintComponent(graphics);
+
+    verify(graphics).fillOval(50, 50, 30, 20);
   }
 }
