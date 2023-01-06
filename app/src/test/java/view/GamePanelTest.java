afedf385b88fb8eebd44cff21861ca4b7cd2f628
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-06 18:29:54 +0100

    [RED] Add test for painting the components of the panel

--- app/src/test/java/view/GamePanelTest.java
+++ app/src/test/java/view/GamePanelTest.java
@@ -5,19 +5,32 @@ import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.awt.Graphics;
 
 public class GamePanelTest {
   private BallView mockBallView;
   private BrickView mockBrickView;
   private PaddleView mockPaddleView;
+  private Graphics mockGraphics;
+  private GamePanel sut;
 
   @BeforeEach void setUp() {
     mockBallView = mock(BallView.class);
     mockBrickView = mock(BrickView.class);
     mockPaddleView = mock(PaddleView.class);
+    mockGraphics = mock(Graphics.class);
+    sut = new GamePanel(mockBallView, mockBrickView, mockPaddleView);
   }
 
   @Test void constructor() {
     assertDoesNotThrow(() -> new GamePanel(mockBallView, mockBrickView, mockPaddleView));
+  }
+
+  @Test void verifyThatComponentsArePainted() {
+    sut.paintComponent();
+
+    verify(mockBallView).paint(mockGraphics);
+    verify(mockBrickView).paint(mockGraphics);
+    verify(mockPaddleView).paint(mockGraphics);
   }
 }
