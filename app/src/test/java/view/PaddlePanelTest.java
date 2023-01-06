be6292a21142adde8543b35ff0ac3fd99974b5a4
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-06 15:54:29 +0100

    [RED] Add test for painting a paddle

--- app/src/test/java/view/PaddlePanelTest.java
+++ app/src/test/java/view/PaddlePanelTest.java
@@ -8,14 +8,31 @@ import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.awt.Graphics;
+
 public class PaddlePanelTest {
   private Paddle mockPaddle;
+  private PaddlePanel sut;
+  private Graphics graphics;
 
   @BeforeEach void setUp() {
     mockPaddle = mock(Paddle.class);
+    sut = new PaddlePanel(mockPaddle);
+    graphics = mock(Graphics.class);
   }
 
   @Test void constructor() {
     assertDoesNotThrow(() -> new PaddlePanel(mockPaddle));
+  }
+
+  @Test void verifyThatPaddleIsPainted() {
+    when(mockPaddle.getWidth()).thenReturn(150);
+    when(mockPaddle.getHeight()).thenReturn(20);
+    when(mockPaddle.getXPos()).thenReturn(325);
+    when(mockPaddle.getYPos()).thenReturn(20);
+
+    sut.paintComponent(graphics);
+
+    verify(graphics).fillRect(325, 20, 150, 20);
   }
 }
