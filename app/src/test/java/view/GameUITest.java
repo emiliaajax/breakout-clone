0e503aaad6f052c39dbebe600e977f8b0f4463e3
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-06 22:05:59 +0100

    [RED] Add test for creating an instance of GameUI

---
+++ app/src/test/java/view/GameUITest.java
@@ -0,0 +1,34 @@
+package view;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.*;
+
+import javax.swing.JFrame;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.Ball;
+import model.Brick;
+import model.Paddle;
+
+public class GameUITest {
+  private static final int WIDTH = 800;
+  private static final int HEIGHT = 600;
+
+  private JFrame mockFrame;
+  private Ball mockBall;
+  private Brick mockBrick;
+  private Paddle mockPaddle;
+
+  @BeforeEach void setUp() {
+    mockFrame = mock(JFrame.class);
+    mockBall = mock(Ball.class);
+    mockBrick = mock(Brick.class);
+    mockPaddle = mock(Paddle.class);
+  }
+
+  @Test void constructor () {
+    assertDoesNotThrow(() -> new GameUI(mockFrame, WIDTH, HEIGHT, mockBall, mockBrick, mockPaddle));
+  }
+}
