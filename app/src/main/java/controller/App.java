b65fbe0b6cdfb22e827dd45739ab2b0f3207686f
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-06 23:35:05 +0100

    [REFACTOR] Add white lines to increase readability

--- app/src/main/java/controller/App.java
+++ app/src/main/java/controller/App.java
@@ -8,6 +8,7 @@ import javax.swing.JFrame;
 import model.Ball;
 import model.Brick;
 import model.Paddle;
+import utils.FrameCommons;
 import view.BallView;
 import view.BrickView;
 import view.GamePanel;
@@ -16,6 +17,18 @@ import view.PaddleView;
 
 public class App {
   public static void main(String[] args) {
-    GameUI gameUI = new GameUI(new JFrame(), 800, 800, new GamePanel(new BallView(new Ball()), new BrickView(new Brick()), new PaddleView(new Paddle())));
+    JFrame frame = new JFrame();
+
+    Ball ball = new Ball();
+    Brick brick = new Brick();
+    Paddle paddle = new Paddle();
+
+    BallView ballView = new BallView(ball);
+    BrickView brickView = new BrickView(brick);
+    PaddleView paddleView = new PaddleView(paddle);
+
+    GamePanel gamePanel = new GamePanel(ballView, brickView, paddleView);
+
+    GameUI gameUI = new GameUI(frame, FrameCommons.WIDTH, FrameCommons.HEIGHT, gamePanel);
   }
 }
