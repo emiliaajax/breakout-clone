57b8eb423030aef02fc0b0a4648d8274e1a05dbc
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-07 00:04:34 +0100

    [GREEN] Implement constructor for Game class

--- app/src/main/java/controller/Game.java
+++ app/src/main/java/controller/Game.java
@@ -1,5 +1,20 @@
 package controller;
 
+import model.Ball;
+import model.Brick;
+import model.Paddle;
+import view.GameUI;
+
 public class Game {
-  
+  private Ball ball;
+  private Brick brick;
+  private Paddle paddle;
+  private GameUI gameUI;
+
+  public Game(Ball ball, Brick brick, Paddle paddle, GameUI gameUI) {
+    this.ball = ball;
+    this.brick = brick;
+    this.paddle = paddle;
+    this.gameUI = gameUI;
+  }
 }
