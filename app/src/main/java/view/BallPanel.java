6a40c9f9738f9537e8f0771cde903437bf00c2ae
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-06 13:45:34 +0100

    [GREEN] Add method for painting the ball

--- app/src/main/java/view/BallPanel.java
+++ app/src/main/java/view/BallPanel.java
@@ -1,11 +1,16 @@
 package view;
 
 import model.Ball;
+import java.awt.Graphics;
 
 public class BallPanel {
   private Ball ball;
 
   public BallPanel(Ball ball) {
     this.ball = ball;
+  }
+
+  public void paintComponent(Graphics graphics) {
+    graphics.fillOval(ball.getXPos(), ball.getYPos(), ball.getWidth(), ball.getHeight());
   }
 }
