47e8355d80aeeea63237e7f37bc0d35dd3fa9e43
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-07 03:43:07 +0100

    [GREEN] Create PaddleAdapter class and create constructor

---
+++ app/src/main/java/view/PaddleAdapter.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.awt.event.KeyAdapter;
+
+import model.Paddle;
+
+public class PaddleAdapter extends KeyAdapter {
+  private Paddle paddle;
+
+  public PaddleAdapter (Paddle paddle) {
+    this.paddle = paddle;
+  }
+}
