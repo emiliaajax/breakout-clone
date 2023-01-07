f90a5bd5e56743c1f928bca94cfac194a0c44444
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-07 03:49:09 +0100

    [RED] Add test for when key is pressed

--- app/src/test/java/view/PaddleAdapterTest.java
+++ app/src/test/java/view/PaddleAdapterTest.java
@@ -7,15 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.*;
 
 import model.Paddle;
+import java.awt.event.KeyEvent;
 
 public class PaddleAdapterTest {
   private Paddle mockPaddle;
+  private KeyEvent mockEvent;
+  private PaddleAdapter sut;
 
   @BeforeEach void setUp() {
     mockPaddle = mock(Paddle.class);
+    mockEvent = mock(KeyEvent.class);
+
+    sut = new PaddleAdapter(mockPaddle);
   }
 
   @Test void constructor() {
     assertDoesNotThrow(() -> new PaddleAdapter(mockPaddle));
+  }
+
+  @Test void testKeyPressed() {
+    sut.keyPressed(mockEvent);
+
+    verify(mockPaddle).onKeyPress(mockEvent);
   }
 }
