0f34cfb71987ca1421d66d463727343d64b55f55
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-08 23:25:32 +0100

    [RED] Add test for creating as many brick views as there are bricks

--- app/src/test/java/controller/GameViewFactoryTest.java
+++ app/src/test/java/controller/GameViewFactoryTest.java
@@ -1,6 +1,7 @@
 package controller;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.Ball;
+import model.Brick;
 import model.Paddle;
 
 public class GameViewFactoryTest {
@@ -31,5 +33,11 @@ public class GameViewFactoryTest {
     Paddle mockPaddle = mock(Paddle.class);
 
     assertNotNull(sut.createPaddleView(mockPaddle));
+  }
+
+  @Test void testCreateBrickViews() {
+    Brick[] mockBricks = mock(Brick[].class);
+
+    assertEquals(sut.createBrickViews(mockBricks).length, mockBricks.length);
   }
 }
