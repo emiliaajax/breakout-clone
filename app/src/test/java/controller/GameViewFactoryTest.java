13507f104d2dda359c24c0c46f1a582c07d4ddd4
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-08 23:18:01 +0100

    [REFACTOR] Move creating of SUT to be performed before each test

--- app/src/test/java/controller/GameViewFactoryTest.java
+++ app/src/test/java/controller/GameViewFactoryTest.java
@@ -4,19 +4,24 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.Ball;
 
 public class GameViewFactoryTest {
+  private GameViewFactory sut;
+
+  @BeforeEach void setUp() {
+    sut = new GameViewFactory();
+  }
   @Test void constructor() {
     assertDoesNotThrow(() -> new GameViewFactory());
   }
 
   @Test void testCreateBallView() {
-    GameViewFactory sut = new GameViewFactory();
     Ball mockBall = mock(Ball.class);
 
     assertNotNull(sut.createBallView(mockBall));
-   }
+  }
 }
