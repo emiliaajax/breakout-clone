104ebe7af899b531ef0299014a0d5ca6d150f174
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-08 22:07:48 +0100

    [GREEN] Implement createPaddle method

--- app/src/main/java/controller/GameObjectFactory.java
+++ app/src/main/java/controller/GameObjectFactory.java
@@ -1,9 +1,14 @@
 package controller;
 
 import model.Ball;
+import model.Paddle;
 
 public class GameObjectFactory {
   public Ball createBall() {
     return new Ball();
+  }
+
+  public Paddle createPaddle () {
+    return new Paddle();
   }
 }
