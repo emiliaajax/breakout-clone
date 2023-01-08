d650db72b880d3269e8ec9c6de40446eaab211c5
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-08 23:35:46 +0100

    [GREEN] Add metod for creating brick views for every brick

--- app/src/main/java/controller/GameViewFactory.java
+++ app/src/main/java/controller/GameViewFactory.java
@@ -1,8 +1,10 @@
 package controller;
 
 import model.Ball;
+import model.Brick;
 import model.Paddle;
 import view.BallView;
+import view.BrickView;
 import view.PaddleView;
 
 public class GameViewFactory {
@@ -12,5 +14,13 @@ public class GameViewFactory {
 
   public PaddleView createPaddleView(Paddle paddle) {
     return new PaddleView(paddle);
+  }
+
+  public BrickView[] createBrickViews(Brick[] bricks) {
+    BrickView[] brickViews = new BrickView[bricks.length];
+    for (int i = 0; i < bricks.length; i++) {
+      brickViews[i] = new BrickView(bricks[i]);
+    }
+    return brickViews;
   }
 }
