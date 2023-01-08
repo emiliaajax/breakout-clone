2d7b18aa6f30f286b244b77b978753165627ebf1
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-08 23:13:51 +0100

    [RED] Add test for creating BallView

--- app/src/test/java/controller/GameViewFactoryTest.java
+++ app/src/test/java/controller/GameViewFactoryTest.java
@@ -1,6 +1,7 @@
 package controller;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,4 +9,10 @@ public class GameViewFactoryTest {
   @Test void constructor() {
     assertDoesNotThrow(() -> new GameViewFactory());
   }
+
+  @Test void testCreateBallView() {
+    GameViewFactory sut = new GameViewFactory();
+
+    assertNotNull(sut.createBallView());
+   }
 }
