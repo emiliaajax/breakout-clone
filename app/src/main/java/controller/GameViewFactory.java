76c60da11944e4102aef2315a3e5c5c1d34dc50f
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-08 23:21:43 +0100

    [GREEN] Add method for creating a PaddleView in factory2

--- app/src/main/java/controller/GameViewFactory.java
+++ app/src/main/java/controller/GameViewFactory.java
@@ -1,10 +1,16 @@
 package controller;
 
 import model.Ball;
+import model.Paddle;
 import view.BallView;
+import view.PaddleView;
 
 public class GameViewFactory {
   public BallView createBallView(Ball ball) {
     return new BallView(ball);
+  }
+
+  public PaddleView createPaddleView(Paddle paddle) {
+    return new PaddleView(paddle);
   }
 }
