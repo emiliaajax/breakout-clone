027edfa915386c0190b9fa748812bcd6573f1c4d
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-08 22:01:08 +0100

    [RED] Add test for creating a ball in factory class

--- app/src/test/java/controller/GameObjectFactoryTest.java
+++ app/src/test/java/controller/GameObjectFactoryTest.java
@@ -1,11 +1,18 @@
 package controller;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
 public class GameObjectFactoryTest {
   @Test void constructor() {
     assertDoesNotThrow(() -> new GameObjectFactory());
+  }
+
+  @Test void testCreateBall() {
+    GameObjectFactory sut = new GameObjectFactory();
+
+    assertNotNull(sut.createBall());
   }
 }
