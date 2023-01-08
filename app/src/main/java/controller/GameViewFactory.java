b930a331ab18361f3e328e0af378484cb2f9991f
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-08 23:16:28 +0100

    [GREEN] Implement method for creating a BallView in factory

--- app/src/main/java/controller/GameViewFactory.java
+++ app/src/main/java/controller/GameViewFactory.java
@@ -1,5 +1,10 @@
 package controller;
 
+import model.Ball;
+import view.BallView;
+
 public class GameViewFactory {
-  
+  public BallView createBallView(Ball ball) {
+    return new BallView(ball);
+  }
 }
--- app/src/test/java/controller/GameViewFactoryTest.java
+++ app/src/test/java/controller/GameViewFactoryTest.java
@@ -2,8 +2,11 @@ package controller;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
+
+import model.Ball;
 
 public class GameViewFactoryTest {
   @Test void constructor() {
@@ -12,7 +15,8 @@ public class GameViewFactoryTest {
 
   @Test void testCreateBallView() {
     GameViewFactory sut = new GameViewFactory();
+    Ball mockBall = mock(Ball.class);
 
-    assertNotNull(sut.createBallView());
+    assertNotNull(sut.createBallView(mockBall));
    }
 }
