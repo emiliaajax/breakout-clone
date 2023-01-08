67c7c50ef510f50d82c711a5e40fb8652dd338d9
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-08 22:04:23 +0100

    [REFACTOR] Move creating sut for test to do it before each test

--- app/src/test/java/controller/GameObjectFactoryTest.java
+++ app/src/test/java/controller/GameObjectFactoryTest.java
@@ -3,16 +3,21 @@ package controller;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameObjectFactoryTest {
+  private GameObjectFactory sut;
+
+  @BeforeEach void setUp() {
+    sut = new GameObjectFactory();
+  }
+
   @Test void constructor() {
     assertDoesNotThrow(() -> new GameObjectFactory());
   }
 
   @Test void testCreateBall() {
-    GameObjectFactory sut = new GameObjectFactory();
-
     assertNotNull(sut.createBall());
   }
 }
