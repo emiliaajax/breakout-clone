d4a62118c1b1a59da13eb939da6403b3bc8d95cd
Emilia Hansson <eh222yn@student.lnu.se>
2023-01-10 08:31:49 +0100

    [GREEN] Add missing test for App

--- app/src/test/java/controller/AppTest.java
+++ app/src/test/java/controller/AppTest.java
@@ -1,10 +1,15 @@
 package controller;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 	@Test void constructor() {
 		assertNotNull(new App());
+	}
+
+	@Test void shouldStartGame() {
+		assertDoesNotThrow(() -> App.main(null));
 	}
 }
